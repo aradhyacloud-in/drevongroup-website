bbb036c8578f68d852216c780b7dc6d6ccdcc2f0
DREVON GROUP <aradhyacloud.in@gmail.com>
2026-05-26 10:33:00 +0530

    Update cart-page.js

--- js/cart-page.js
+++ js/cart-page.js
@@ -42,8 +42,20 @@ function renderCartPage() {
         <div class="cart-info">
           <h3>${item.name}</h3>
 
-          <p>Quantity: ${item.quantity}</p>
+         <div class="cart-quantity">
 
+           <button onclick="decreaseCartQuantity(${index})">
+             −
+           </button>
+         
+           <span>${item.quantity}</span>
+         
+           <button onclick="increaseCartQuantity(${index})">
+             +
+           </button>
+         
+         </div>
+          
           <p>${item.price}</p>
 
           <button onclick="removeFromCart(${index})">
@@ -71,4 +83,44 @@ function removeFromCart(index) {
   localStorage.setItem("cart", JSON.stringify(cart));
 
   renderCartPage();
+}
+/* =========================================
+   INCREASE CART QUANTITY
+========================================= */
+
+function increaseCartQuantity(index) {
+
+  let cart =
+    JSON.parse(localStorage.getItem("cart")) || [];
+
+  cart[index].quantity++;
+
+  localStorage.setItem("cart", JSON.stringify(cart));
+
+  renderCartPage();
+
+  updateCartCount();
+}
+
+
+/* =========================================
+   DECREASE CART QUANTITY
+========================================= */
+
+function decreaseCartQuantity(index) {
+
+  let cart =
+    JSON.parse(localStorage.getItem("cart")) || [];
+
+  if (cart[index].quantity > 1) {
+    cart[index].quantity--;
+  } else {
+    cart.splice(index, 1);
+  }
+
+  localStorage.setItem("cart", JSON.stringify(cart));
+
+  renderCartPage();
+
+  updateCartCount();
 }
